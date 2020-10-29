f20a39cfcd45f6c075db2fff36484208d4cf4a10
Vlpalamar <65705755+Vlpalamar@users.noreply.github.com>
2020-10-29 13:23:02 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Play = new System.Windows.Forms.Button();
+            this.IsTrueLabel = new System.Windows.Forms.Label();
+            this.CountLabel = new System.Windows.Forms.Label();
+            this.Replay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(258, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(292, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter u nuber";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(258, 186);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(148, 23);
+            this.Play.TabIndex = 2;
+            this.Play.Text = "Enter";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // IsTrueLabel
+            // 
+            this.IsTrueLabel.AutoSize = true;
+            this.IsTrueLabel.Location = new System.Drawing.Point(498, 156);
+            this.IsTrueLabel.Name = "IsTrueLabel";
+            this.IsTrueLabel.Size = new System.Drawing.Size(0, 13);
+            this.IsTrueLabel.TabIndex = 3;
+            this.IsTrueLabel.Visible = false;
+            // 
+            // CountLabel
+            // 
+            this.CountLabel.AutoSize = true;
+            this.CountLabel.Location = new System.Drawing.Point(45, 155);
+            this.CountLabel.Name = "CountLabel";
+            this.CountLabel.Size = new System.Drawing.Size(0, 13);
+            this.CountLabel.TabIndex = 4;
+            // 
+            // Replay
+            // 
+            this.Replay.Location = new System.Drawing.Point(258, 186);
+            this.Replay.Name = "Replay";
+            this.Replay.Size = new System.Drawing.Size(148, 23);
+            this.Replay.TabIndex = 5;
+            this.Replay.Text = "Play Again";
+            this.Replay.UseVisualStyleBackColor = true;
+            this.Replay.Visible = false;
+            this.Replay.Click += new System.EventHandler(this.Replay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Replay);
+            this.Controls.Add(this.CountLabel);
+            this.Controls.Add(this.IsTrueLabel);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Label IsTrueLabel;
+        private System.Windows.Forms.Label CountLabel;
+        private System.Windows.Forms.Button Replay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int i;
+        int randi;
+        public Random rand = new Random();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            randi=rand.Next(0, 2000);
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        int count = 0;
+        private void Play_Click(object sender, EventArgs e)
+        {
+            i =Convert.ToInt32(textBox1.Text);
+            IsTrueLabel.Visible = true;
+            
+            CountLabel.Text += (++count).ToString() + ". " + i.ToString()+". " ;
+            if (i<0 || i>2000)
+            {
+                IsTrueLabel.Text = "nuber is not in a range,try again";
+                CountLabel.Text += "not in a range";
+                
+            }
+            else if (i==randi)
+            {
+                IsTrueLabel.Text = $"Congratulation, u won\n\n u were needed in { count}Tries ";
+                CountLabel.Text += "True";
+                Play.Visible = false;
+                Replay.Visible = true;
+               
+                
+
+            }
+            else if(i< randi)
+            {
+                IsTrueLabel.Text = "u number is less then computer's number,try again";
+                CountLabel.Text += "Need Large";
+               
+            }
+            else if (i > randi)
+            {
+                IsTrueLabel.Text = "u number is large then computer's number,try again";
+                CountLabel.Text += "Need Less";
+            }
+            CountLabel.Text += "\n";
+
+        }
+
+        private void Replay_Click(object sender, EventArgs e)
+        {
+            Replay.Visible = false;
+            CountLabel.Text = "";
+            count = 0;
+            randi = rand.Next(0, 2000);
+            textBox1.Text = "";
+            IsTrueLabel.Text = "";
+            Play.Visible = true;
+        }
+    }
+}
